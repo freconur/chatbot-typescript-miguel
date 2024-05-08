60b333b4ce6f16b8e81228074f10e48e5c86f074
Franco <freconur@gmail.com>
2024-05-07 19:07:02 -0500

    agregando nuexo executable path

--- src/app.ts
+++ src/app.ts
@@ -100,7 +100,7 @@ const flujoPagoVerificacion = addKeyword(['verificacion', 'verificar'])
                             "--single-process",
                             "--no-zygote"
                           ],
-                          executablePath: "/usr/bin/google-chrome-stable",
+                          executablePath: puppeteer.executablePath(),
                           // ignoreHTTPSErrors: true,
                           // slowMo:3000
                         })
